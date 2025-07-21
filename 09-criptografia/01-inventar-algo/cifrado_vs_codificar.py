57c41181e1b61f9bbe9cd348fcf9546d6ecbfc7f
CosmoCarpincho <cosmocarpincho@gmail.com>
2025-07-20 21:29:46 -0300

    study python 26

---
+++ 09-criptografia/01-inventar-algo/cifrado_vs_codificar.py
@@ -0,0 +1,31 @@
+# cifrar es símbolo a símbolo
+
+cifrado01 = {
+    'a': 'b',
+    'b': 'c',
+    'c': 'd',
+}
+
+def cifrar_siguiente_caracter(texto: str) -> str:
+    texto_cifrado = ""
+    for c in texto:
+        texto_cifrado = texto_cifrado + chr(ord(c) + 1)
+    return texto_cifrado
+
+def descifrar_siguiente_caracter(texto: str) -> str:
+    texto_descifrado = ""
+    for c in texto:
+        texto_descifrado = texto_descifrado + chr(ord(c) - 1)
+    return texto_descifrado
+
+
+texto = "Hola"
+cifrado = cifrar_siguiente_caracter(texto)
+print(cifrado)
+
+descifrado = descifrar_siguiente_caracter(cifrado)
+print(descifrado)
+
+
+
+# codificar es palabra a otra
