c1b5499656ee33edbc52ecdb6cc911bda9d59063
CosmoCarpincho <cosmocarpincho@gmail.com>
2025-09-15 19:16:21 -0300

    study python 40

--- 05-libro-curo-intensivo-python/09-clases/ej/ej9-1.py
+++ 05-libro-curo-intensivo-python/09-clases/ej/ej9-1.py
@@ -15,3 +15,30 @@ class Restaurante:
 r = Restaurante("Los manolos", "buffet")
 r.describir_restaurante()
 r.abrir_restaurante()
+
+r2 = Restaurante("Argenchina", "Comida China")
+r3 = Restaurante("El señor de los novillos", "Asado")
+
+r2.describir_restaurante()
+r3.describir_restaurante()
+
+class Usuario:
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+    
+    def describir_usuario(self):
+        return f"El usuario de nombre: {self.nombre} y apellido {self.apellido}"
+
+    def saludar_usuario(self):
+        print(f"Buenas {self.nombre}")
+    
+
+cosmo = Usuario("Cosmo", "Carpincho")
+pepe = Usuario("Pepe", "Argento")
+
+print(cosmo.describir_usuario())
+cosmo.saludar_usuario()
+
+print(pepe.describir_usuario())
+pepe.saludar_usuario()
